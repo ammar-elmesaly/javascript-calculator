08aff17761db424709645fdf05b345036adf7ca0
Ammar Elmesaly <amarmazn4@gmail.com>
2025-05-31 12:05:26 +0300

    Finish calculator (unsafe)

--- js/script.js
+++ js/script.js
@@ -3,23 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultScreen = document.querySelector('.result');
     let error = false;
 
-    function updateResult(value) {  // value could be a number or an operator
-        if (!error) {
-            resultScreen.innerHTML += value;
+    function insertValue(value) {  // value could be a number or an operator
+        if (!error && resultScreen.textContent.length < 15) {
+            resultScreen.textContent += value;
         }
     }
 
     function evaluateResult() {
         if (!error) {
             try {
-                let calculation = cleanFloat(eval(resultScreen.innerHTML));
-                resultScreen.innerHTML = calculation;
+                let calculation = cleanFloat(math.evaluate(resultScreen.textContent));
+                resultScreen.textContent = calculation;
                 if (Number.isNaN(calculation)) {
-                    resultScreen.innerHTML = '(Math Error)';
+                    resultScreen.textContent = '(Math Error)';
                     error = true;
                 }
             } catch(err) {
-                resultScreen.innerHTML = '(Syntax Error)';
+                resultScreen.textContent = '(Syntax Error)';
                 error = true;
             }
         }
@@ -27,20 +27,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function backspace() {
         if (!error) {
-            resultScreen.innerHTML = resultScreen.innerHTML.slice(0, -1);  // removes last character
+            resultScreen.textContent = resultScreen.textContent.slice(0, -1);  // removes last character
         }
     }
 
     function clearResult() {
-        resultScreen.innerHTML = '';
+        resultScreen.textContent = '';
         error = false;
     }
 
-    function cleanFloat(num, precision=12) {
-        return parseFloat(num.toPrecision(precision));
+    function cleanFloat(num, floatPrecision=13, expPrecision=10) {
+        if (num > Math.pow(10, expPrecision)) {
+            let result = Number(num).toExponential(expPrecision-1);
+            return result.replace(/(\.\d*?[1-9])0+|\.0+(?=e)|\.0+$/, '$1');  // Clean trailing zeros
+        }
+        return parseFloat(num.toPrecision(floatPrecision));
     }
 
-    window.updateResult = updateResult;
+    window.insertValue = insertValue;
     window.evaluateResult = evaluateResult;
     window.backspace = backspace;
     window.clearResult = clearResult;
